06fb5f931d5184628506e9d27887bc70c16e5018
“Tyler <tschoppe@umass.edu>
2019-02-26 10:53:57 -0500

    Adding Spring Boot

---
+++ server/src/main/java/com/diningoinc/diningo/DiningoApplication.java
@@ -0,0 +1,13 @@
+package com.diningoinc.diningo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiningoApplication.class, args);
+	}
+
+}
