19e69d4471d12a11b5eebaa17e966d59252468f4
NickRose30 <nicholasrose30@gmail.com>
2019-04-29 23:58:53 -0400

    Page not found styling

--- client/src/components/PageNotFound.js
+++ client/src/components/PageNotFound.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+import { colorDefaultOrange } from '../vars';
+
+const Page = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: ${colorDefaultOrange};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Logo = styled.div`
+  color: white;
+  font-size: 400%;
+`;
+
+const Message = styled.div`
+  color: white;
+  font-size: 300%;
+`;
 
 const PageNotFound = () => (
-  <h1>404 Page Not Found</h1>
+  <Page>
+    <Logo>DININGO</Logo>
+    <Message>404 Page Not Found</Message>
+  </Page>
 );
 
 export default PageNotFound;
