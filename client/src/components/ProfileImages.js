206900b18ce4d9e5da0d89bec6f4d499b0bff158
NickRose30 <nicholasrose30@gmail.com>
2019-04-29 23:45:08 -0400

    image gallery

--- client/src/components/ProfileImages.js
+++ client/src/components/ProfileImages.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { colorTransparentBlack } from '../vars';
+import { IoMdArrowDropright, IoMdArrowDropleft } from 'react-icons/io';
 
 const SectionContainer = styled.div`
   display: flex;
@@ -32,24 +34,70 @@ const SmallImage = styled.div`
   }
 `;
 
+const NextButton = styled.div`
+  color: white;
+  font-size: xx-large;
+  padding: 10px 0 5px 0;
+  float: right;
+  margin-top: -50%;
+  background: ${colorTransparentBlack};
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const PrevButton = styled.div`
+  color: white;
+  font-size: xx-large;
+  padding: 10px 0 5px 0;
+  float: left;
+  margin-top: -50%;
+  background: ${colorTransparentBlack};
+
+  :hover {
+    cursor: pointer;
+  }
+`;
 
 class ProfileImages extends Component {
   state = {
     mainImageIndex: 0,
   }
 
-  handleClick = i => () => this.setState({ mainImageIndex: i });
+  handleImgClick = i => () => this.setState({ mainImageIndex: i });
+
+  handleNextClick = numImages => () => this.setState(prevState => ({
+    mainImageIndex: 
+      prevState.mainImageIndex < numImages - 1 ? prevState.mainImageIndex + 1 : 0
+  })); 
+
+  handlePrevClick = numImages => () => this.setState(prevState => ({
+    mainImageIndex: 
+      prevState.mainImageIndex > 0 ? prevState.mainImageIndex - 1 : numImages - 1
+  }));
 
   render() {
+    const { images } = this.props;
+    const numImgs = images.length;
+
     const defaultImg = this.props.images[this.state.mainImageIndex];
 
     const smallImages = this.props.images.map((src, i) => (
-      <SmallImage src={src} onClick={this.handleClick(i)} key={i} />
+      <SmallImage src={src} onClick={this.handleImgClick(i)} key={i} />
     ));
 
     return (
       <SectionContainer>
-        <LargeImage src={defaultImg} />
+        <div>
+          <LargeImage src={defaultImg} />
+          <NextButton onClick={this.handleNextClick(numImgs)}>
+            <IoMdArrowDropright />
+          </NextButton>
+          <PrevButton onClick={this.handlePrevClick(numImgs)}>
+            <IoMdArrowDropleft />
+          </PrevButton>
+        </div>
         <SmallImagesContainer>
           {smallImages}
         </SmallImagesContainer>
